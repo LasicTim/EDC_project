94a836a325d375d64341dc198e5e61a0832a3726
Tim <lasictim@gmail.com>
2025-02-26 14:47:01 +0100

    Added option for source code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,11 +20,12 @@ const App: React.FC = () => {
           init={{
             height: 400,
             menubar: false,
-            plugins: "advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount ",
+            plugins:
+              "advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount",
             toolbar:
               "undo redo | formatselect | bold italic backcolor | \
               alignleft aligncenter alignright alignjustify | \
-              bullist numlist outdent indent | removeformat | help",
+              bullist numlist outdent indent | removeformat | code | help",
           }}
           onEditorChange={(newContent) => setContent(newContent)}
         />
