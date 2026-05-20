94018cdd383b94b755fecac2b4295cd3bd62385a
Tim <lasictim@gmail.com>
2026-05-20 13:56:11 +0200

    added jinja2 metrics

--- backend/app/reporting/jinja/report.py
+++ backend/app/reporting/jinja/report.py
@@ -1,3 +1,6 @@
+import time
+
+import psutil
 from jinja2 import Template
 from bs4 import BeautifulSoup
 import os
@@ -93,13 +96,56 @@ class Report_V2:
             file_name += ext
         return file_name
 
-    def render(self, context: dict):
-        rendered_content = self.template.render(context)
+    @staticmethod
+    def used_memory():
+        process = psutil.Process(os.getpid())
+        return process.memory_info().rss
 
+    def render(self, context: dict):
+        start_time = time.perf_counter()
+        start_mem = self.used_memory()
+        # FILL
+        start_fill_time = time.perf_counter()
+        start_fill_mem = self.used_memory()
+        rendered_content = self.template.render(context)
+        end_fill_time = time.perf_counter()
+        end_fill_mem = self.used_memory()
+
+        fill_time_ms = (end_fill_time - start_fill_time) * 1000
+        fill_memory_used = end_fill_mem - start_fill_mem
+        # EXPORT
+        start_export_time = time.perf_counter()
+        start_export_mem = self.used_memory()
         if self.output_format == "html":
             with open(self.output_file_name, "w", encoding="utf-8") as f:
                 f.write(rendered_content)
         elif self.output_format == "pdf":
             pdfkit.from_string(rendered_content, self.output_file_name)
+
+        end_export_time = time.perf_counter()
+        end_export_mem = self.used_memory()
+
+        export_time_ms = (end_export_time - start_export_time) * 1000
+        export_memory_used = end_export_mem - start_export_mem
+
+        # TOTAL
+        end_time = time.perf_counter()
+        end_mem = self.used_memory()
+
+        total_time_ms = (end_time - start_time) * 1000
+        total_memory_used = end_mem - start_mem
+
+        print("\n--- Jinja2 PERFORMANCE METRICS ---")
+
+        print(f"fill_time_ms: {fill_time_ms:.2f}")
+        print(f"fill_memory_used_bytes: {fill_memory_used}")
+
+        print(f"export_time_ms: {export_time_ms:.2f}")
+        print(f"export_memory_used_bytes: {export_memory_used}")
+
+        print(f"total_time_ms: {total_time_ms:.2f}")
+        print(f"total_memory_used_bytes: {total_memory_used}")
+
+        print("-----------------------------------")
 
         self.url = f"{BACKEND_URL}/{self.output_file_name}"
